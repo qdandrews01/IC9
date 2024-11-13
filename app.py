cffb0a199c7ed9c78dbd8342f976a659ccc53e8e
Quontaurus Andrews <136196117+qdandrews01@users.noreply.github.com>
2024-11-12 19:11:23 -0600

    second dev second fix

--- app.py
+++ app.py
@@ -35,10 +35,5 @@ def get_chocolates():
     return jsonify(chocolates)
 
 
-@app.route('/seconddevchanges', methods=['GET'])
-def get_chocolates():
-    return "Hello, it is second developer"
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
